ca4d1b807fbcfe1cc0118e4f1a2b01871379ee68
marco <mvcantero1@up.edu.ph>
2024-09-08 11:22:49 +0800

    Updated wallet_to_bs58

--- wallet_to_bs58.ts
+++ wallet_to_bs58.ts
@@ -1,12 +1,12 @@
 import bs58 from "bs58";
-(async () => {
-  const PRIVATE_KEY =
-    "5gQWXvxghHYQKLm2F4vAa4JKE87YdcynKrkWT2AuheFLDKS3EAjLDWZHUkVmkov3gNRQLh6p6VSRvfc9HGjEHcZn";
-  const decoded = bs58.decode(PRIVATE_KEY);
 
+(async () => {
+  // my own private key
+  const PRIVATE_KEY = "5C6qtgp6QEXRh5MFWHL8jb8aUZ7x38hrzzp68xgEi9vRtzVQxGwoAEE2JfjtYjJAPTvKTKeUttg9HP4wdqUEh4Qb";
+  
+  const decoded = bs58.decode(PRIVATE_KEY);
   console.log(decoded);
 
   const encoded = bs58.encode(decoded);
-
   console.log(encoded);
 })();
